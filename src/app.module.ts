880e48975c0ac9318881a31a2bf4cf3921bd2321
kaeky <carlosbasan2011@hotmail.com>
2025-01-09 09:25:33 -0500

    feat: create and config database connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { typeOrmConfig } from './config/database.config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ClientModule } from './client/client.module';
+import { CreditModule } from './credit/credit.module';
 
 @Module({
-  imports: [],
+  imports: [TypeOrmModule.forRoot(typeOrmConfig), ClientModule, CreditModule],
   controllers: [AppController],
   providers: [AppService],
 })
