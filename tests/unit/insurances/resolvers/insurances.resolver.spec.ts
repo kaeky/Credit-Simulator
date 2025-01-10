48e0340fdb261590d642d53c894d8c9233bf9c13
kaeky <carlosbasan2011@hotmail.com>
2025-01-10 06:48:44 -0500

    feat: insurance and controller unit test

--- tests/unit/insurances/resolvers/insurances.resolver.spec.ts
+++ tests/unit/insurances/resolvers/insurances.resolver.spec.ts
@@ -1,19 +1,111 @@
+import { InsurancesResolver } from '../../../../src/insurances/resolvers/insurances.resolver';
+import { InsurancesService } from '../../../../src/insurances/services/insurances.service';
 import { Test, TestingModule } from '@nestjs/testing';
-import { InsurancesResolver } from '../../../../insurances/resolvers/insurances.resolver';
-import { InsurancesService } from '../../../../insurances/services/insurances.service';
+import { InsuranceEntity } from '../../../../src/insurances/entities/insurance.entity';
+import { CreateInsuranceDto } from '../../../../src/insurances/dto/create-insurance.dto';
+import { UpdateInsuranceDto } from '../../../../src/insurances/dto/update-insurance.dto';
 
 describe('InsurancesResolver', () => {
   let resolver: InsurancesResolver;
+  let service: jest.Mocked<InsurancesService>;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [InsurancesResolver, InsurancesService],
+      providers: [
+        InsurancesResolver,
+        {
+          provide: InsurancesService,
+          useValue: {
+            getInsurances: jest.fn(),
+            saveInsurance: jest.fn(),
+            updateInsurance: jest.fn(),
+          },
+        },
+      ],
     }).compile();
 
     resolver = module.get<InsurancesResolver>(InsurancesResolver);
+    service = module.get(InsurancesService);
   });
 
-  it('should be defined', () => {
-    expect(resolver).toBeDefined();
+  describe('getInsurances', () => {
+    it('should return a list of insurances', async () => {
+      const mockInsurances: InsuranceEntity[] = [
+        {
+          id: 1,
+          minAge: 19,
+          maxAge: 30,
+          percentage: 3,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          id: 2,
+          minAge: 31,
+          maxAge: 60,
+          percentage: 4,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+      ];
+
+      service.getInsurances.mockResolvedValue(mockInsurances);
+
+      const result = await resolver.getInsurances();
+
+      expect(service.getInsurances).toHaveBeenCalled();
+      expect(result).toEqual(mockInsurances);
+    });
+  });
+
+  describe('createInsurance', () => {
+    it('should create a new insurance', async () => {
+      const input: CreateInsuranceDto = {
+        minAge: 4,
+        maxAge: 8,
+        percentage: 12.56,
+      };
+
+      const mockInsurance: InsuranceEntity = {
+        id: 1,
+        ...input,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      };
+
+      service.saveInsurance.mockResolvedValue(mockInsurance);
+
+      const result = await resolver.createInsurance(input);
+
+      expect(service.saveInsurance).toHaveBeenCalledWith(input);
+      expect(result).toEqual(mockInsurance);
+    });
+  });
+
+  describe('updateInsurance', () => {
+    it('should update an existing insurance', async () => {
+      const input: UpdateInsuranceDto = {
+        id: 1,
+        minAge: 4,
+        maxAge: 8,
+        percentage: 21,
+      };
+
+      const mockInsurance: InsuranceEntity = {
+        id: 1,
+        minAge: 4,
+        maxAge: 8,
+        percentage: 21,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      };
+
+      service.updateInsurance.mockResolvedValue(mockInsurance);
+
+      const result = await resolver.updateInsurance(input);
+
+      expect(service.updateInsurance).toHaveBeenCalledWith(input);
+      expect(result).toEqual(mockInsurance);
+    });
   });
 });
--- tests/unit/insurances/services/insurances.service.spec.ts
+++ tests/unit/insurances/services/insurances.service.spec.ts
@@ -1,18 +1,132 @@
+import { InsurancesService } from '../../../../src/insurances/services/insurances.service';
+import { InsuranceRepository } from '../../../../src/insurances/repositories/insurance.repository';
 import { Test, TestingModule } from '@nestjs/testing';
-import { InsurancesService } from '../../../../insurances/services/insurances.service';
+import { InsuranceEntity } from '../../../../src/insurances/entities/insurance.entity';
+import { CreateInsuranceDto } from '../../../../src/insurances/dto/create-insurance.dto';
+import { UpdateInsuranceDto } from '../../../../src/insurances/dto/update-insurance.dto';
 
 describe('InsurancesService', () => {
   let service: InsurancesService;
+  let repository: jest.Mocked<InsuranceRepository>;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [InsurancesService],
+      providers: [
+        InsurancesService,
+        {
+          provide: InsuranceRepository,
+          useValue: {
+            find: jest.fn(),
+            getInsuranceByAge: jest.fn(),
+            save: jest.fn(),
+          },
+        },
+      ],
     }).compile();
 
     service = module.get<InsurancesService>(InsurancesService);
+    repository = module.get(InsuranceRepository);
   });
 
-  it('should be defined', () => {
-    expect(service).toBeDefined();
+  describe('getInsurances', () => {
+    it('should return a list of insurances', async () => {
+      const mockInsurances: InsuranceEntity[] = [
+        {
+          id: 1,
+          minAge: 19,
+          maxAge: 30,
+          percentage: 3,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          id: 2,
+          minAge: 31,
+          maxAge: 60,
+          percentage: 4,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+      ];
+
+      repository.find.mockResolvedValue(mockInsurances);
+
+      const result = await service.getInsurances();
+
+      expect(repository.find).toHaveBeenCalled();
+      expect(result).toEqual(mockInsurances);
+    });
+  });
+
+  describe('getInsuranceByAge', () => {
+    it('should return an insurance based on age', async () => {
+      const mockInsurance: InsuranceEntity = {
+        id: 1,
+        minAge: 31,
+        maxAge: 60,
+        percentage: 4,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      };
+      const age = 35;
+
+      repository.getInsuranceByAge.mockResolvedValue(mockInsurance);
+
+      const result = await service.getInsuranceByAge(age);
+
+      expect(repository.getInsuranceByAge).toHaveBeenCalledWith(age);
+      expect(result).toEqual(mockInsurance);
+    });
+  });
+
+  describe('saveInsurance', () => {
+    it('should save a new insurance', async () => {
+      const input: CreateInsuranceDto = {
+        minAge: 4,
+        maxAge: 8,
+        percentage: 12.56,
+      };
+
+      const mockInsurance: InsuranceEntity = {
+        id: 1,
+        ...input,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      };
+
+      repository.save.mockResolvedValue(mockInsurance);
+
+      const result = await service.saveInsurance(input);
+
+      expect(repository.save).toHaveBeenCalledWith(input);
+      expect(result).toEqual(mockInsurance);
+    });
+  });
+
+  describe('updateInsurance', () => {
+    it('should update an existing insurance', async () => {
+      const input: UpdateInsuranceDto = {
+        id: 1,
+        minAge: 4,
+        maxAge: 8,
+        percentage: 21,
+      };
+
+      const mockInsurance: InsuranceEntity = {
+        id: 1,
+        minAge: 4,
+        maxAge: 8,
+        percentage: 21,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      };
+
+      repository.save.mockResolvedValue(mockInsurance);
+
+      const result = await service.updateInsurance(input);
+
+      expect(repository.save).toHaveBeenCalledWith(input);
+      expect(result).toEqual(mockInsurance);
+    });
   });
 });
